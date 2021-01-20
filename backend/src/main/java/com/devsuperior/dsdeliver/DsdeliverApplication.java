a2b31cbfd0703477eec44e37465c808cec4b52f3
Rebeca <bkdias@gmail.com>
2021-01-20 15:38:59 -0300

    Domain Model

--- backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.com.dsdeliver;
+package com.devsuperior.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
