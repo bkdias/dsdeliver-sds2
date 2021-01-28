5a2b44bc70b2c8011f3cf50fdd12b61931cf5578
Rebeca <bkdias@gmail.com>
2021-01-27 21:14:16 -0300

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,16 +1,18 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg'
+import Footer from '../Footer';
 
 function Home(){
     return (
-        <div className="home-container">
+        <>
+            <div className="home-container">
             <div className="home-content">
                 <div className="home-actions">
                     <h1 className="home-title">
                         Faça seu pedido <br/>que entregamos <br/> pra você!!!
                     </h1>
                     <h3>
-                        Escolha o seu pedido e em poucos minutos <br/> levaremoss na sua porta
+                        Escolha o seu pedido e em poucos minutos <br/> levaremos na sua porta
                     </h3>
                     <a className="home-btn-order" href="orders">
                         FAZER PEDIDO
@@ -19,10 +21,11 @@ function Home(){
                 <div className="home-image">
                     <MainImage />
                 </div>
-
             </div>
+            </div>
+            <Footer />
 
-        </div>
+        </>
     );
 }
 
